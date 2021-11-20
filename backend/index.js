94e6953f0351f881781aec7eb06bab8869929bf7
Marcos Felipe <nodmarcos@gmail.com>
2021-11-20 00:08:22 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,10 @@ app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(expressValidator());
 
-consign().include('routes').include('utils').into(app);
+consign()
+    .include('routes')
+    .include('utils')
+    .into(app);
 
 app.listen(4000, '127.0.0.1', ()=>{
 
